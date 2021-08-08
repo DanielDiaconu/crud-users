9db5a3ea06e8eb26db775f8cd8296a2192d05f00
Daniel Diaconu <daniel.diaconu234@gmail.com>
2021-08-08 19:19:30 +0300

    project completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let newUser = {};
+let isEditing = false;
+let editedUser = {};
 
 async function getData() {
   let response = await fetch("http://localhost:3000/users");
@@ -14,6 +16,7 @@ function buildUsers(user) {
   const parent = document.querySelector("tbody");
   const mainDiv = document.createElement("tr");
   mainDiv.classList.add("odd");
+  mainDiv.setAttribute("id", `user-${user.id}`);
   const td = document.createElement("td");
   mainDiv.appendChild(td);
   const tdDiv = document.createElement("div");
@@ -31,9 +34,11 @@ function buildUsers(user) {
   secondaryTd.classList.add("d-flex", "align-items-center");
   mainDiv.appendChild(secondaryTd);
   const secondaryTdDiv = document.createElement("div");
+  secondaryTdDiv.setAttribute("id", "populated");
   secondaryTdDiv.classList.add("d-flex", "flex-column");
   secondaryTd.appendChild(secondaryTdDiv);
   const name = document.createElement("span");
+  name.setAttribute("id", "nameEdit");
   name.classList.add("text-gray-800");
   name.classList.add("text-hover-primary");
   name.classList.add("mb-1");
@@ -43,6 +48,7 @@ function buildUsers(user) {
   email.innerText = user.email;
   secondaryTdDiv.appendChild(email);
   const thirdTd = document.createElement("td");
+  thirdTd.setAttribute("id", `role-${user.id}`);
   thirdTd.innerText = user.role;
   mainDiv.appendChild(thirdTd);
   const dateTd = document.createElement("td");
@@ -65,36 +71,81 @@ function buildUsers(user) {
   parent.appendChild(mainDiv);
 
   del.addEventListener("click", deleteData);
-  edit.addEventListener("click", editInfo);
-  function editInfo(e) {
-    const nameValue = document.getElementById("name_input");
-    const emailValue = document.getElementById("email_input");
-    emailValue.value = email.innerText;
-    nameValue.value = name.innerText;
-  }
+  edit.addEventListener("click", () => editInfo(user));
+}
+
+async function editInfo(user) {
+  isEditing = true;
+  let response = await fetch(`http://localhost:3000/users/${user.id}`);
+  let data = await response.json();
+
+  const nameValue = document.getElementById("name_input");
+  const emailValue = document.getElementById("email_input");
+  const roleValue = document.querySelector(
+    `.form-check-input.me-3[value = "${data.role}"]`
+  );
+  roleValue.setAttribute("checked", "checked");
+  emailValue.value = data.email;
+  nameValue.value = data.name;
+  editedUser = { ...data };
+}
+
+function openAddUserModal() {
+  isEditing = false;
+  const roles = document.querySelectorAll(`.form-check-input.me-3`);
+  roles.forEach((input) => input.removeAttribute("checked"));
+  const form = document.getElementById("kt_modal_add_user_form");
+  // const roleValue = document.querySelector(`.form-check-input.me-3`);
+  // roleValue.setAttribute("checked", "checked");
+  form.reset();
 }
 
 async function createUser(e) {
   e.preventDefault();
-  let response = await fetch("http://localhost:3000/users", {
-    method: "POST",
-    body: JSON.stringify(newUser),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  });
 
-  let data = await response.json();
-  newUser.id = data.id;
-  buildUsers(newUser);
-  const inputs = document.querySelectorAll(".form-control");
-  for (let key of inputs) {
-    key.value = "";
+  if (!isEditing) {
+    let response = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    let data = await response.json();
+    newUser.id = data.id;
+    buildUsers(newUser);
+    const inputs = document.querySelectorAll(".form-control");
+    for (let key of inputs) {
+      key.value = "";
+    }
+  } else {
+    const nameValue = document.getElementById("name_input");
+    const emailValue = document.getElementById("email_input");
+    const roleValue = newUser.role;
+
+    editedUser.name = nameValue.value;
+    editedUser.email = emailValue.value;
+    editedUser.role = roleValue;
+    const { id } = editedUser;
+    await fetch(`http://localhost:3000/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(editedUser),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const userRow = document.getElementById(`user-${id}`);
+    userRow.querySelector("#nameEdit").innerText = editedUser.name;
+    userRow.querySelector("span:not(#nameEdit)").innerText = editedUser.email;
+    userRow.querySelector(`#role-${id}`).innerText = editedUser.role;
+    console.log(newUser);
   }
 }
 
 function onUserDataChange(e) {
   newUser[e.target.name] = e.target.value;
+  console.log(newUser);
 }
 
 async function deleteData(event) {
